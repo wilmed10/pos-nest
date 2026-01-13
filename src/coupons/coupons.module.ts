e7b4b174816f06f36545c7fc1bf33adc1a791958
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-13 13:53:01 -0500

    feat: integrate TypeORM for Coupon entity and implement create method in CouponsService

--- src/coupons/coupons.module.ts
+++ src/coupons/coupons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CouponsService } from './coupons.service';
 import { CouponsController } from './coupons.controller';
+import { Coupon } from './entities/coupon.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coupon])],
   controllers: [CouponsController],
   providers: [CouponsService],
 })
