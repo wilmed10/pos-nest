cf7fb7fdb5ef653e8a1172a7546cce2fb62cb98c
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-13 13:46:20 -0500

    feat: define create coupon dto

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from "class-validator"
+
+export class CreateCouponDto {
+    @IsNotEmpty({message: 'El nombre del cupón es obligatorio'})
+    name: string
+
+    @IsNotEmpty({message: 'El descuento no puede estar vacío'})
+    @IsInt({message: 'El descuento debe ser entero, entre 1 y 100'})
+    @Max(100, {message: 'El descuento máximo es 100'})
+    @Min(1, {message: 'El descuento mínimo es de 1'})
+    percentage: number
+
+    @IsNotEmpty({message: 'La fecha no puede ir vacía'})
+    @IsDateString({}, {message: 'Fecha no válida'})
+    expirationDate: Date
+}
