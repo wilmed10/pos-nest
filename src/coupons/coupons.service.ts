e3650e9fc6a17bbfb7ce9b20d8c6b0c54af83527
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-13 22:34:53 -0500

    feat: add apply coupon functionality with validation and DTO

--- src/coupons/coupons.service.ts
+++ src/coupons/coupons.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Coupon } from './entities/coupon.entity';
 import { Repository } from 'typeorm';
+import { endOfDay, isAfter } from 'date-fns';
 
 @Injectable()
 export class CouponsService {
@@ -37,5 +38,23 @@ export class CouponsService {
     const coupon = await this.findOne(id)
     await this.couponRepository.remove(coupon)
     return {message: 'Cupon eliminado'}
+  }
+
+  async applyCoupon(name: string) {
+    const coupon = await this.couponRepository.findOneBy({name})
+    if(!coupon) {
+      throw new NotFoundException(`El cupón ${name} no existe`)
+    }
+    const currentDate = new Date()
+    const expirationDate = endOfDay(coupon.expirationDate)
+
+    if(isAfter(currentDate, expirationDate)) {
+      throw new UnprocessableEntityException('Cupon ya expirado')
+    }
+
+    return {
+      message: ' Cupon valido',
+      ...coupon
+    }
   }
 }
