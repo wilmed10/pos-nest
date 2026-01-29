26bb6972a6a39ee63335e2134e85e4312b7af97e
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-28 22:26:22 -0500

    fix: update product service delete response

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -93,6 +93,6 @@ export class ProductsService {
   async remove(id: number) {
     const product = await this.findOne(id)
     await this.productRepository.remove(product)
-    return `Producto eliminado`;
+    return {message: 'Producto eliminado'};
   }
 }
